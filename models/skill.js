f79e274d052142d9595f2f57d5e4c0b4207c9d21
MARLIN K MAKORI <marlin.makori@gmail.com>
2022-03-16 19:16:01 -0700

    create & show

---
+++ models/skill.js
@@ -0,0 +1,23 @@
+const skills = ["Functional Programming", "Asynchronous Programming", "Node JS", "jQuery", "HTML / CSS", "Express Js", "React Js", "MongoDB", "Java", "Django", "Python", "Ruby", "Ruby on Rails" ]
+module.exports = {
+    allSkills,
+    showOne,
+    addSkills,
+    updtSkills,
+    frgtSkills
+}
+function allSkills () {
+    return skills
+}
+function showOne(id) {
+  return skills.find((skillEl, idx) => idx == id)  
+}
+function addSkills(skill){
+  return skills.push(`${skill}`)
+}
+function updtSkills(id, skill) {
+  return skills.splice(id, 1, skill)
+}
+function frgtSkills(id) {
+  return skills.splice(id,1)
+}
